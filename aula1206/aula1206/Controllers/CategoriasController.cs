e0ab4e852b782baa9ad36118afd08dc6c5dd8db8
Marcello Markwardt <marsensacao@hotmail.com>
2017-06-12 10:48:47 -0300

    validação

--- aula1206/aula1206/Controllers/CategoriasController.cs
+++ aula1206/aula1206/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aula1206.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,17 +12,29 @@ namespace aula1206.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
-            List<string> categorias = new List<string>();
-
-            categorias.Add("Carros");
-            categorias.Add("Motos");
-            categorias.Add("Barcos");
-            categorias.Add("Aviões");
-            categorias.Add("Caminhões");
+            List<Categoria> categorias = new List<Categoria>();
+            categorias.Add(new Categoria() { Nome = "Carros", Descricao="Super carros do momento", Ativo=true });
+            categorias.Add(new Categoria() { Nome = "Motos", Descricao="As mais estilosas do mundo"});
+            categorias.Add(new Categoria() { Nome = "Barcos", Descricao="Super lançamentos de 2017"});
+            categorias.Add(new Categoria() { Nome = "Aviões", Descricao="Aviões do exercito" });
+            categorias.Add(new Categoria() { Nome = "Caminhões", Descricao="Varias marcas" });
+                
 
             ViewBag.ListaCategorias = categorias;
 
-            return View();
+            return View(categorias);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View(); 
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            return View(categoria);
         }
     }
 }
