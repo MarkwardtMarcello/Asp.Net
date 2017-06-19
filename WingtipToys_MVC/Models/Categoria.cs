d805057bcc34282236a2fad4d2c55c67a7cad7c0
Marcello Markwardt <marsensacao@hotmail.com>
2017-06-19 11:38:11 -0300

    Projeto Novo Scaffolder

---
+++ WingtipToys_MVC/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModels
+{
+    public class Categoria
+    {
+
+        [Key]
+        public int CategoriaID { get; set; }
+
+        [Required(ErrorMessage = "Inserir Nome!!"),
+            StringLength(10, ErrorMessage = "Tamanho maximo é 10 caracteres")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição"),
+            DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
---
+++ WingtipToys_MVC/Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModels
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoID { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+        [Display(Name ="Descrição")]
+        [DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+
+        [Required]
+        [Display(Name ="Preço")]
+        [DataType(DataType.Currency)]
+        public decimal Preco { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public virtual Categoria categoria { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+
+
+    }
+}
